576ed6a67cffdf3730522e143570f01ea4d66cd0
josanne <a.j.verheule@students.uu.nl>
2015-04-28 10:54:57 +0000

    1

--- Euler/Euler2.py
+++ Euler/Euler2.py
@@ -1,18 +1,6 @@
-'''total = 0
-def fib(n):
-    if n < 3:
-        return 1
-    return fib(n-1) + fib(n - 2)
-    
-for n in range(10):
-    if fib(n)<4000000:
-        if fib(n) % 2 == 0:
-            total += fib(n)
-print(total)'''
-
 total = 0
 a = b = 1
-while a < 40:
+while a < 4000000:
     if a % 2 == 0:
         total += a
     c = a + b
