3b9bda79139ed005329792cadae440a5ea74408b
josanne <a.j.verheule@students.uu.nl>
2015-04-28 07:35:37 +0000

    Laatste versie

--- Euler/Euler2.py
+++ Euler/Euler2.py
@@ -1,4 +1,22 @@
+'''total = 0
+def fib(n):
+    if n < 3:
+        return 1
+    return fib(n-1) + fib(n - 2)
+    
+for n in range(10):
+    if fib(n)<4000000:
+        if fib(n) % 2 == 0:
+            total += fib(n)
+print(total)'''
 
-for n in range(400):
-    n+=1
-    print(n)
+total = 0
+a = b = 1
+while a < 40:
+    if a % 2 == 0:
+        total += a
+    c = a + b
+    b = a
+    a = c
+
+print(total)
