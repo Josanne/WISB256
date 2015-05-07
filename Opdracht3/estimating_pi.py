94ee02811e31f03cc34ed95dcfd2b59059c27aad
josanne <a.j.verheule@students.uu.nl>
2015-05-07 14:05:20 +0000

    Mapje Opdrach3 toegevoegd met bijbehorende bestanden

---
+++ Opdracht3/estimating_pi.py
@@ -0,0 +1,33 @@
+import random
+import math
+import sys
+
+if len(sys.argv) < 3:
+    print("Use: estimate_pi.py N L")
+    exit()
+if len(sys.argv) > 3:
+    random.seed(int(sys.argv[3]))
+    
+
+def drop_needle(L):
+    x0 = random.random()
+    a = random.vonmisesvariate(0,0)
+    x1 = x0 + L*math.cos(a)
+    if x1 > 1 or x1 < 0:
+        return True
+    else:
+        return False
+
+
+L = float(sys.argv[2])
+assert L <= 1, "L should be smaller than 1"
+
+count = 0
+N = int(sys.argv[1]) 
+for i in range(N+1):
+    if drop_needle(L) == True:
+        count += 1
+
+pi = float((2 * L * N) / count)
+print(count, "hits in" , N , "tries") 
+print("Pi =", pi)
