602b431784d87fa697b175e62c9055dd273312ee
josanne <a.j.verheule@students.uu.nl>
2015-05-21 13:15:44 +0000

    Met bonus

---
+++ Euler/Euler3.py
@@ -0,0 +1,7 @@
+N = 300
+getallen = 
+
+for i in range(1,N+1):
+    for k in getallen:
+        if (N+1-i) % k != 0:
+            print (N+1-i)
--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -1,3 +1,4 @@
+import math
 def findRoot(f,a,b,epsilon):
     m = float((a+b)/2)
     if abs(b-a) < epsilon:
@@ -8,5 +9,21 @@ def findRoot(f,a,b,epsilon):
         else:
             return findRoot(f,m,b,epsilon)
             
-root = findRoot(lambda x:x-1,0,3,.1)
+root = findRoot(lambda x:math.cos(x),0,3,.1)
 print(root)
+
+def findAllRoots(f,a,b,epsilon):
+    m = float((a+b)/2)
+    roots = []
+    if abs(b-a) < epsilon:
+        roots.append(m)
+        return roots
+    else: 
+        if f(a)*f(m)<=0:
+            roots.extend(findAllRoots(f,a,m,epsilon))
+        if f(b)*f(m)<=0:
+            roots.extend(findAllRoots(f,m,b,epsilon))
+        
+        return roots
+x = findAllRoots(lambda x:math.sin(x),1,9,.1)
+print(x)
